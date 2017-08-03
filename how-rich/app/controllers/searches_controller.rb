dbf18f94c84fdd8faae1d02193c5c7af448494fd
github username <github email address>
2017-08-03 11:52:52 -0400

    add user searches

--- how-rich/app/controllers/searches_controller.rb
+++ how-rich/app/controllers/searches_controller.rb
@@ -6,34 +6,34 @@ class SearchesController < ApplicationController
   def new
     @all_searches = Search.all
     if current_user
-      current_user.searches << @search
       @user_searches = Search.where(user_id: "#{current_user.id}")
     end
 
   end
 
   def create
-    @search = Search.create(search_params)
+    @search = Search.new(search_params)
     @stock = Stock.find_by(stock_params)
     # make sure ticker_symbols are up case when inputted
-    @stock.searches << @search
     stock_quote = MarketBeat.quotes(@stock.ticker_symbol, @search.start_date, @search.end_date)
-    @search.update(sell_price: stock_quote.first[:low].to_f, buy_price: stock_quote.last[:high].to_f)
+    byebug
     if @search.start_date < stock_quote.last[:date]
       flash[:message] = "#{@stock.name} was not around at that time!"
       render :new
-    elsif @search.buy_price > @search.investment_amount
+    elsif stock_quote.last[:high].to_f > @search.investment_amount
       flash[:message] = "That isn't enough to buy one share! The price per share on that day was #{@search.buy_price}"
       render :new
     else
+      @search.save
+      @stock.searches << @search
+      @search.update(sell_price: stock_quote.first[:low].to_f, buy_price: stock_quote.last[:high].to_f)
       Calculation.new(@search)
-      @all_searches = Search.all
       if current_user
         current_user.searches << @search
         @user_searches = Search.where(user_id: "#{current_user.id}")
       end
+        @all_searches = Search.all
       render :stock_search
-
     end
   end
 
--- how-rich/app/controllers/users_controller.rb
+++ how-rich/app/controllers/users_controller.rb
@@ -22,7 +22,8 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find_by(id: params[:id])
-
+    @user_searches = Search.where(user_id: @user.id)
+    @all_searches = Search.all
   end
 
   def destroy
